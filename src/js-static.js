3c085448242859060d02e171bd772673979d49cb
L2jLiga <L2jLiga@mail.ru>
2017-10-11 21:45:43 +0700

    Isolate scope of appLogic Isolate scope of bootstrap logic Squeeze max-width of containers to 1200px Squeeze max-width of slider to 1440px

--- src/js-static.js
+++ src/js-static.js
@@ -2,16 +2,24 @@
   'use strict';
 
   module.exports = [
-    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/jquery/dist/jquery.js',
 
     './node_modules/popper.js/dist/umd/popper.js',
+
+    './src/isolate-scope-start.js',
 
     './node_modules/bootstrap/js/dist/util.js',
 
     './node_modules/bootstrap/js/dist/dropdown.js',
 
+    './src/isolate-scope-end.js',
+
+    './src/isolate-scope-start.js',
+
     './src/js-libs/**/*.js',
 
     './src/js/**/*.js',
+
+    './src/isolate-scope-end.js',
   ];
 })()
